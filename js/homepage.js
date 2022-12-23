858107c7daba0c24c90ff510d4e5e77faa5d762b
fares berrani <fberrani@hotmail.fr>
2022-12-23 12:27:04 +0100

    slider fini

--- js/homepage.js
+++ js/homepage.js
@@ -2,3 +2,26 @@ function darkMode(){
     var element = document.body;
     element.classList.toggle("dark")
 }
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    freeMode: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+});
+
+
+var elementTwo = document.getElementsByTagName('body')
+
+function scroll(){
+    
+    console.log(document.body.onscroll)
+}
+
+
+
+
+
